6de2cce3d7b8ac31dd8daf370505e7f622fb1aa4
DenVik <denyagarkusha2@gmail.com>
2025-02-18 22:52:33 +0100

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { header } from './js/header';
-import { faq } from './js/faq';
-import { gallery } from './js/gallery';
-import { reviews } from './js/reviews';
+import { header } from './js/header.js';
+import { faq } from './js/faq.js';
+import { gallery } from './js/gallery.js';
+import { reviews } from './js/reviews.js';
 
 header();
 faq();
