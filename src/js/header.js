a553e2086f01f1ba20a5621b4def891c65712f5a
DenVik <denyagarkusha2@gmail.com>
2025-02-19 13:46:43 +0100

    header and scroll-up have done

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,15 @@
 export const header = () => {
-  //code
+  const openMenuBtn = document.querySelector('.burger_menu-js');
+  const menu = document.querySelector('.mobile-nav-js');
+  const menuItems = document.querySelectorAll('.mobile-menu-item-js');
+
+  openMenuBtn.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+  });
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('is-open');
+    });
+  });
 };
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { header } from './js/header.js';
 import { faq } from './js/faq.js';
 import { gallery } from './js/gallery.js';
 import { reviews } from './js/reviews.js';
+import { scrollUp } from './js/scrollUp.js';
 
 header();
 faq();
 gallery();
 reviews();
+scrollUp()
