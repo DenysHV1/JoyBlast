679e5ec045e692ee8f69f4396be9ebfbc35f22bb
Yuliia Voziian <vozianulia@gmail.com>
2025-02-21 02:17:20 +0200

    add gallery section

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,47 @@
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+import "swiper/css/pagination";
+
 export const gallery = () => {
-  //code
-}
+  let swiperInstance = null;
+
+  const initSwiper = () => {
+    if (window.innerWidth < 1200) {
+      if (!swiperInstance) {
+        swiperInstance = new Swiper(".swiper", {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          centeredSlides: true,
+          loop: true,
+          grabCursor: true,
+          speed: 800,
+          simulateTouch: true,
+          touchRatio: 1,
+          mousewheel: {
+            sensitivity: 0.5,
+          },
+          keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+            clickable: true,
+          },
+        });
+      }
+    } else {
+
+      if (swiperInstance) {
+        swiperInstance.destroy(true, true);
+        swiperInstance = null;
+      }
+    }
+  };
+
+
+  initSwiper();
+
+  window.addEventListener('resize', initSwiper);
+};
