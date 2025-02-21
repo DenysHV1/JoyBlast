f943131aabadcef6f3714d59fff50fdf2c37c2e2
DenVik <denyagarkusha2@gmail.com>
2025-02-21 11:13:57 +0100

    swiper section  fix

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -14,6 +14,7 @@ export const gallery = () => {
           centeredSlides: true,
           loop: true,
           grabCursor: true,
+          spaceBetween: 10,
           speed: 800,
           simulateTouch: true,
           touchRatio: 1,
